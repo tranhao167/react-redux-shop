82445c859c666f6302d09ddc1ff6810d83788d26
Zheng UET <zhenguet@gmail.com>
2020-08-21 09:50:29 +0700

    create product component

--- react-redux-shop/src/components/Products.js
+++ react-redux-shop/src/components/Products.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import formatCurrency from '../utill'; 
 
 export default class Products extends Component {
-  render() {
+  render () {
     return (
       <div>
-        <ul className='products'>
-          {this.props.products.map((product) => (
+        <ul className="products">
+          {this.props.products.map (product => (
             <li key={product._id}>
-              <div className='product'>
+              <div className="product">
                 <a href={'#' + product._id}>
-                  <img src={product.image} alt={product.title}></img>
+                  <img src={product.image} alt={product.title} />
                   <p>{product.title}</p>
                 </a>
-                <div className='product-price'>
-                  <div>{product.price}</div>
-                  <button className='button primary'>Add to cart</button>
+                <div className="product-price">
+                  <div>{formatCurrency (product.price)}</div>
+                  <button className="button primary">Add to cart</button>
                 </div>
               </div>
             </li>
