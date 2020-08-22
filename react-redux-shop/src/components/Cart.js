db64f82d30d93c66466e6eb2266423e45e06570b
Zheng UET <zhenguet@gmail.com>
2020-08-22 12:01:20 +0700

    Revert "Revert "Update Cart.js""
    This reverts commit 4c41652dcf41fd33fdc4f12c87082ba3278812b0.

--- react-redux-shop/src/components/Cart.js
+++ react-redux-shop/src/components/Cart.js
@@ -23,6 +23,7 @@ export default class Cart extends Component {
                     <div>{item.title}</div>
                     <div className="right">
                       {formatCurrency (item.price)} x {item.count}{' '}
+
                       <button
                         className="button"
                         onClick={() => this.props.removeFromCart (item)}
@@ -35,6 +36,18 @@ export default class Cart extends Component {
               ))}
             </ul>
           </div>
+          {cartItems.length !== 0 &&
+            <div className="cart">
+              <div className="total">
+                <div>
+                  Total:{''}
+                  {formatCurrency (
+                    cartItems.reduce ((a, c) => a + c.price * c.count, 0)
+                  )}
+                </div>
+                <button className="button primary">Proceed</button>
+              </div>
+            </div>}
         </div>
       </div>
     );
